617230aa9c1a4115193f90f20c0f865a8c1f6b7e
Viviane Qian <viviane.qian@grenoble-inp.org>
2023-04-20 09:51:32 +0200

    extract user fixtures

--- backendsqlite/src/core/usecases/User/RegisterUser.spec.ts
+++ backendsqlite/src/core/usecases/User/RegisterUser.spec.ts
@@ -1,6 +1,7 @@
 import EncryptionInMemory from "../../../adapters/driven/EncryptionInMemory";
 import UserRepositoryInMemory from "../../../adapters/driven/repositories/UserRepositoryInMemory";
 import User from "../../../domain/entities/User";
+import UserFixtures from "../../../domain/entities/User.fixtures";
 import RegisterUser from "./RegisterUser";
 
 const userRepository = new UserRepositoryInMemory();
@@ -22,7 +23,7 @@ const thenUsersShouldBe = async (users: User[]) => {
 describe('Feature: Register an user', () => {
     test('Example: Jean wants to register as the first user', async () => {
         givenUsers([]);
-        await whenNewUserRegisters(new User(1, 'Jean', 'jean@mail.com'), '123456');
-        await thenUsersShouldBe([new User(1, 'Jean', 'jean@mail.com', 'hash')]);
+        await whenNewUserRegisters(UserFixtures.userJean, '123456');
+        await thenUsersShouldBe([UserFixtures.userJeanWithHash]);
     });
 });
