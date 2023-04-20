3f0fd9f9e79a6e3e27389809a0c0eda15c5837c2
Viviane Qian <viviane.qian@grenoble-inp.org>
2023-04-20 10:30:42 +0200

    extract functions to dbUtils

--- backendsqlite/src/__tests__/api.user.test.js
+++ backendsqlite/src/__tests__/api.user.test.js
@@ -1,7 +1,17 @@
 const app = require("../adapters/driving/app");
 const request = require("supertest");
+const { cleanDb, seedDb } = require("../util/dbUtils");
+
+let anUser;
 
 describe("e2e: /api/users", () => {
+  beforeAll(async () => {
+    const data = await seedDb();
+    anUser = data.anUser;
+  });
+  afterAll(async () => {
+    await cleanDb();
+  });
   describe("POST /api/users/register", () => {
     test("Example: sends a request with name, email and password", async () => {
       const response = await request(app).post("/api/users/register").send({
@@ -27,7 +37,45 @@ describe("e2e: /api/users", () => {
         email: "test@email.com",
       });
       expect(response.statusCode).toBe(400);
-      expect(response.body.message).toBe("You must specify the name, email and password");
+      expect(response.body.message).toBe(
+        "You must specify the name, email and password"
+      );
+    });
+  });
+
+  describe("POST /api/users/signin", () => {
+    test("Example: sends a request with correct email and password", async () => {
+      const response = await request(app).post("/api/users/signin").send({
+        email: anUser.email,
+        password: anUser.password,
+      });
+      expect(response.statusCode).toBe(200);
+      expect(response.body.message).toBe("User signed in");
+    });
+    test("Example: sends a request with correct email and wrong password", async () => {
+      const response = await request(app).post("/api/users/signin").send({
+        email: anUser.email,
+        password: 'dummy password',
+      });
+      expect(response.statusCode).toBe(403);
+      expect(response.body.message).toBe("Wrong email or password");
+    });
+    test("Example: sends a request with wrong email and password", async () => {
+      const response = await request(app).post("/api/users/signin").send({
+        email: 'dummy email',
+        password: 'dummy password',
+      });
+      expect(response.statusCode).toBe(403);
+      expect(response.body.message).toBe("Wrong email or password");
+    });
+    test("Example: sends a request with email and no password", async () => {
+      const response = await request(app).post("/api/users/signin").send({
+        email: "test@email.com",
+      });
+      expect(response.statusCode).toBe(400);
+      expect(response.body.message).toBe(
+        "You must specify the email and password"
+      );
     });
   });
 });
--- backendsqlite/src/util/updatedb.js
+++ backendsqlite/src/util/updatedb.js
@@ -1,19 +1,13 @@
-const models = require("../adapters/driven/models/index.js");
-const bcrypt = require("bcrypt");
-const db = require("../adapters/driven/models/database.js");
+const seedDb = require("./dbUtils").seedDb;
+const cleanDb = require("./dbUtils").cleanDb;
 // Ajouter ici les nouveaux require des nouveaux modèles
 
 // eslint-disable-next-line no-unexpected-multiline
 (async () => {
   // Regénère la base de données
-  await db.sync({ force: true });
+  await cleanDb();
   console.log("Base de données créée.");
   // Initialise la base avec quelques données
-  const passhash = await bcrypt.hash("123456", 2);
-  await models.users.create({
-    name: "Sebastien Viardot",
-    email: "Sebastien.Viardot@grenoble-inp.fr",
-    passhash,
-  });
+  await seedDb();
   // Ajouter ici le code permettant d'initialiser par défaut la base de donnée
 })();
