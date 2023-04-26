4f2bd1dee0ea895043f5b32d9e7402a6952e79e9
Antoine Klein <antoineklein2000@gmail.com>
2023-04-26 22:09:34 +0200

    test wait-on

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "http://localhost:19006",
     setupNodeEvents(on, config) {
       // implement node event listeners here
       // what could be here? : https://nodejs.org/api/process.html#process_process_events
