0eff849075ac2fdf07fd039036d57d55df0a7044
Antoine Klein <antoineklein2000@gmail.com>
2023-04-29 20:04:42 +0200

    init navigation bar

--- frontend/pages/HomePage.js
+++ frontend/pages/HomePage.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { Button } from 'react-native-paper';
+import { View, StyleSheet } from 'react-native';
+import { Button, BottomNavigation, Text } from 'react-native-paper';
 import { setToken } from '../store/slices/authSlice'
 import { useDispatch, useSelector } from 'react-redux';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const MusicRoute = () => <Text>Music</Text>;
+
+const AlbumsRoute = () => <Text>Albums</Text>;
+
+const RecentsRoute = ()  => {
+    return ( <Text>Recents</Text>);
+};
+
+const NotificationsRoute = () => <Text>Notifications</Text>;
 
 
 const HomePage = ({ navigation }) => {
@@ -14,8 +24,24 @@ const HomePage = ({ navigation }) => {
         AsyncStorage.removeItem('token');
         navigation.navigate('Landing');
     };
+
+    const [index, setIndex] = React.useState(0);
+    const [routes] = React.useState([
+        { key: 'discover', title: 'Favorites', focusedIcon: 'heart', unfocusedIcon: 'heart-outline' },
+        { key: 'albums', title: 'Albums', focusedIcon: 'message', badge: 10 },
+        { key: 'recents', title: 'Recents', focusedIcon: 'history', badge: true },
+        { key: 'notifications', title: 'Notifications', focusedIcon: 'bell', unfocusedIcon: 'bell-outline' },
+    ]);
+
+    const renderScene = BottomNavigation.SceneMap({
+        music: MusicRoute,
+        albums: AlbumsRoute,
+        recents: RecentsRoute,
+        notifications: NotificationsRoute,
+    });
+
     return (
-        <View>
+        <View style={{ flex: 1, justifyContent: 'flex-end' }}>
             <Text>Home Page</Text>
             {token &&
                 <Button
@@ -23,9 +49,21 @@ const HomePage = ({ navigation }) => {
                     mode="contained"
                 >Se déconnecter</Button>
             }
+            <BottomNavigation style={styles.navigationBar}
+                navigationState={{ index, routes }}
+                onIndexChange={setIndex}
+                renderScene={renderScene}
+            />
         </View>
     );
 };
 
+const styles = StyleSheet.create({
+    navigationBar: {
+        borderTopWidth: 4   
+
+    },
+});
+        
 
 export default HomePage;
