d06b028c91bfe5ee68666d0ba9de5ce2876aa6e6
Viviane Qian <viviane.qian@grenoble-inp.org>
2023-05-05 09:20:17 +0200

    fix size and spacing

--- frontend/pages/LandingPage.js
+++ frontend/pages/LandingPage.js
@@ -1,68 +1,64 @@
-import React, {useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Text, Button} from 'react-native-paper';
+import { Text, Button } from 'react-native-paper';
 import Icon from 'react-native-paper/src/components/Icon';
-import { useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 
-
-const LandingPage = ({navigation}) => {
-    const { token } = useSelector((state) => state.auth);
-	useEffect(() => {
-        if (token) {
-          navigation.navigate('Home'); 
-        }
-    }, [token]);
-    return (
-        <View style={styles.container}>
-            <Icon source="account-group" size={30}/>
-            <Text style={styles.title}>BuddyBazaar</Text>
-            <View style={styles.options}>
-                <Button
-                    onPress={() => navigation.navigate('Register')}
-                    mode="outlined"
-                    icon="account-multiple-plus"
-                    style={styles.button}
-                >Inscription</Button>
-                <Button
-                    onPress={() => navigation.navigate('Login')}
-                    mode="contained"
-                    icon="login"
-                    style={styles.button}
-                >Connexion</Button>
-                <Button
-                    onPress={() => navigation.navigate('Home')}
-                    mode="outlined"
-                    icon="map-search"
-                    style={styles.button}
-                >Découvrir</Button>
-            </View>
-        </View>
-    );
+const LandingPage = ({ navigation }) => {
+  const { token } = useSelector((state) => state.auth);
+  useEffect(() => {
+    if (token) {
+      navigation.navigate('Home');
+    }
+  }, [token]);
+  return (
+    <View style={styles.container}>
+      <Icon source="account-group" size={30} />
+      <Text style={styles.title}>BuddyBazaar</Text>
+      <View style={styles.buttonsContainer}>
+        <Button
+          onPress={() => navigation.navigate('Register')}
+          mode="outlined"
+          icon="account-multiple-plus"
+        >
+          Inscription
+        </Button>
+        <Button
+          onPress={() => navigation.navigate('Login')}
+          mode="contained"
+          icon="login"
+        >
+          Connexion
+        </Button>
+        <Button
+          onPress={() => navigation.navigate('Home')}
+          mode="outlined"
+          icon="map-search"
+        >
+          Découvrir
+        </Button>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#fff',
-    },
-    title: {
-        fontSize: 24,
-        marginBottom: 30,
-    },
-    options: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        height: '20%',
-    },
-    button: {
-        width: '100%',
-    }
-    
-
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 30,
+  },
+  buttonsContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 30,
+  },
 });
 
 export default LandingPage;
--- frontend/pages/RegisterPage.js
+++ frontend/pages/RegisterPage.js
@@ -7,132 +7,146 @@ import { setName } from '../store/slices/authSlice';
 import { registerUser } from '../store/thunks/authThunk';
 
 const RegisterPage = () => {
-    const dispatch = useDispatch();
-    const [ snackbarVisible, setSnackbarVisible ] = useState(false);
-    const [ snackbarType, setSnackbarType ] = useState('error');
-    const [ snackbarMessage, setSnackbarMessage ] = useState('Une erreur est survenue');
-    const { email, name } = useSelector((state) => state.auth);
-    const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const [snackbarVisible, setSnackbarVisible] = useState(false);
+  const [snackbarType, setSnackbarType] = useState('error');
+  const [snackbarMessage, setSnackbarMessage] = useState(
+    'Une erreur est survenue'
+  );
+  const { email, name } = useSelector((state) => state.auth);
+  const [password, setPassword] = useState('');
 
-    const handleRegister = () => {
-        // Check if all fields are filled
-        if(!email || !name || !password){
-            setSnackbarVisible(true);
-            setSnackbarType('error');
-            setSnackbarMessage('Tous les champs doivent être remplis');
-            return;
-        }
-        // Check if password is valid (at least 8 characters, one uppercase, one lowercase, one number and one special character)
-        if(/^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}$/.test(password) === false){
-            setSnackbarVisible(true);
-            setSnackbarType('error');
-            setSnackbarMessage('Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial');
-            return;
-        }
-        const res = dispatch(registerUser({password}));
-        res.then((res) => {
-            setSnackbarVisible(true);
-            if(!res.payload){
-                setSnackbarType('error');
-                setSnackbarMessage('Une erreur est survenue');
-                return;
-            }
-            if(res.payload.error){
-                setSnackbarType('error');
-                setSnackbarMessage("L'adresse email est déjà utilisée");
-                return;
-            }
-            if(!res.payload.error){
-                setSnackbarType('success');
-                setSnackbarMessage("Vous êtes inscrit, vous pouvez maintenant vous connecter");
-                // Clear fields
-                dispatch(setEmail(''));
-                dispatch(setName(''));
-                setPassword('');
-                return;
-            }
+  const handleRegister = () => {
+    // Check if all fields are filled
+    if (!email || !name || !password) {
+      setSnackbarVisible(true);
+      setSnackbarType('error');
+      setSnackbarMessage('Tous les champs doivent être remplis');
+      return;
+    }
+    // Check if password is valid (at least 8 characters, one uppercase, one lowercase, one number and one special character)
+    if (
+      /^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}$/.test(
+        password
+      ) === false
+    ) {
+      setSnackbarVisible(true);
+      setSnackbarType('error');
+      setSnackbarMessage(
+        'Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial'
+      );
+      return;
+    }
+    const res = dispatch(registerUser({ password }));
+    res.then((res) => {
+      setSnackbarVisible(true);
+      if (!res.payload) {
+        setSnackbarType('error');
+        setSnackbarMessage('Une erreur est survenue');
+        return;
+      }
+      if (res.payload.error) {
+        setSnackbarType('error');
+        setSnackbarMessage("L'adresse email est déjà utilisée");
+        return;
+      }
+      if (!res.payload.error) {
+        setSnackbarType('success');
+        setSnackbarMessage(
+          'Vous êtes inscrit, vous pouvez maintenant vous connecter'
+        );
+        // Clear fields
+        dispatch(setEmail(''));
+        dispatch(setName(''));
+        setPassword('');
+        return;
+      }
+    });
+  };
 
-        });
-    };
+  const handleEmail = (text) => {
+    dispatch(setEmail(text));
+  };
+  const handleName = (text) => {
+    dispatch(setName(text));
+  };
 
-    const handleEmail = (text) => {
-        dispatch(setEmail(text));
-    };
-    const handleName = (text) => {
-        dispatch(setName(text));
-    };
-
-    return (
-        <View style={styles.container}>
-            <Snackbar
-                    visible={snackbarVisible}
-                    onDismiss={() => setSnackbarVisible(false)}
-                    style={snackbarType === 'error' ? styles.error : styles.success}
-                    action={{
-                        label: '⨯',
-                        onPress: () => {
-                            setSnackbarVisible(false)
-                        },
-                    }}
-                   >
-                    {snackbarMessage}
-                </Snackbar>
-            <View style={styles.form}>
-                <View style={styles.inputContainer}>
-                    <TextInput
-                        label="Votre nom"
-                        placeholder="Votre nom"
-                        onChangeText={handleName}
-                        value={name}
-                    />
-                    <TextInput
-                        label="Adresse email"
-                        placeholder="Adresse email"
-                        onChangeText={handleEmail}
-                        value={email}
-                    />
-                    <TextInput
-                        label="Mot de passe"
-                        placeholder="Mot de passe"
-                        secureTextEntry={true}
-                        onChangeText={setPassword}
-                        value={password}
-                    />
-                </View>
-                <Button
-                    onPress={handleRegister}
-                    mode="outlined"
-                    icon="account-multiple-plus"   
-                >S&apos;inscrire</Button>
-            </View>
+  return (
+    <View style={styles.container}>
+      <Snackbar
+        visible={snackbarVisible}
+        onDismiss={() => setSnackbarVisible(false)}
+        style={snackbarType === 'error' ? styles.error : styles.success}
+        action={{
+          label: '⨯',
+          onPress: () => {
+            setSnackbarVisible(false);
+          },
+        }}
+      >
+        {snackbarMessage}
+      </Snackbar>
+      <View style={styles.form}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            label="Votre nom"
+            placeholder="Votre nom"
+            onChangeText={handleName}
+            value={name}
+            mode="outlined"
+          />
+          <TextInput
+            label="Adresse email"
+            placeholder="Adresse email"
+            onChangeText={handleEmail}
+            value={email}
+            mode="outlined"
+          />
+          <TextInput
+            label="Mot de passe"
+            placeholder="Mot de passe"
+            secureTextEntry={true}
+            onChangeText={setPassword}
+            value={password}
+            mode="outlined"
+          />
         </View>
-    );
+        <Button
+          onPress={handleRegister}
+          mode="outlined"
+          icon="account-multiple-plus"
+        >
+          S&apos;inscrire
+        </Button>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#fff',
-    },
-    form: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-evenly',
-        alignItems: 'center',
-        height: '50%',
-    },
-    button: {
-        width: '100%',
-    },
-    error: {
-        backgroundColor: '#e35d6a',
-    },
-    success: {
-        backgroundColor: '#479f76',
-    }
-
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    height: '50%',
+    gap: 24,
+  },
+  inputContainer: {
+    minWidth: 200,
+    gap: 10,
+  },
+  error: {
+    backgroundColor: '#e35d6a',
+  },
+  success: {
+    backgroundColor: '#479f76',
+  },
 });
 
 export default RegisterPage;
