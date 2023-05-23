3a2694f9db52d97687b1e971c14e741089a7d25a
Antoine Klein <antoineklein2000@gmail.com>
2023-05-23 16:46:28 +0200

    added demo values

--- backendsqlite/src/util/updatedb.js
+++ backendsqlite/src/util/updatedb.js
@@ -1,4 +1,5 @@
 const seedDb = require('./dbUtils').seedDb;
+const seeDbProd = require('./dbUtils').seedDbProd;
 const cleanDb = require('./dbUtils').cleanDb;
 // Ajouter ici les nouveaux require des nouveaux modèles
 
@@ -8,6 +9,9 @@ const cleanDb = require('./dbUtils').cleanDb;
   await cleanDb();
   console.log('Base de données créée.');
   // Initialise la base avec quelques données
-  await seedDb();
+  if(process.env.NODE_ENV === 'production')
+    await seeDbProd();
+  else
+    await seedDb();
   // Ajouter ici le code permettant d'initialiser par défaut la base de donnée
 })();
